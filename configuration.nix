75e3ad52bcee4a9289be18e55d5a58c035ae1f58
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2025-12-26 15:48:10 -0600

    Revert "remove gnome:"
    This reverts commit ddf6a8a5c90026606ff567c18c5b6503f77e383d.

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,13 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
@@ -68,6 +75,9 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kusha = {
