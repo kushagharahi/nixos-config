b7791b83bcfc7f6e0c19e985d63d76e488ec7853
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2026-01-18 20:37:45 -0600

    udiskie

--- configuration.nix
+++ configuration.nix
@@ -161,6 +161,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     git
+    udiskie # automount usb drives
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
